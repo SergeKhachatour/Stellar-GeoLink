7a60e021dfdcddaeaf755ff09784e984292509ce
Serge Khachatour <sergekhachatour@gmail.com>
2025-10-24 13:46:50 -0700

    Fix all Navbar components to use correct Navigation/Navbar
    - Fixed Layout.js to use Navigation/Navbar instead of old Navbar.js
    - Removed force deployment message from index.html
    - Ensures all navigation components use /docs links
    - Covers both mobile and desktop navigation across all pages

--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import { Box } from '@mui/material';
-import Navbar from './Navbar';
+import Navbar from './Navigation/Navbar';
 
 const Layout = () => {
     return (
